ba734c739dca95b74de172e7a5b76a1c50329974
Hassaan Ali <hassaanali994@gmail.com>
2025-08-15 10:55:28 +0500

    First_Chapter_js

---
+++ app.js
@@ -0,0 +1,59 @@
+// 1. Write a script to greet your website visitor using JS alert 
+// box.
+
+// alert("hello sir")
+
+
+
+
+
+// 2. Write a script to display following message on your web 
+// page:
+
+// document.write("hello Java <br>")
+
+
+
+
+
+// 3. Write a script to display following message on your web 
+// page: (Hint : Use line break) 
+
+// document.write("hello Java <br>")
+// document.write("Hello Hassaan")
+
+
+
+
+// 4. Write a script to display following messages in sequence:
+
+// alert("your prompt")
+// alert("your 2nd prompt")
+
+
+
+// 5. Generate the following message through browser’s 
+// developer console:
+
+// console.log("Hello its my first class web development");
+
+
+
+// 6. Make use of alerts in your new/existing HTML & CSS 
+// project.
+
+// function showMessage(){
+//     alert("this is my HTML & CSS project")
+// }
+
+
+
+// 7. Practice placement of <script></script> element in 
+// following sections of your project in exercise 6: 
+// a. Head 
+// b. Body (before your page’s HTML)
+// c. Body (inside your page’s HTML) 
+// d. Body (after your page’s HTML)             // go to html file 
+
+
+
