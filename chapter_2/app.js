402cc7824d924240a4a1636c0f6c012b2e9d12c2
Hassaan Ali <hassaanali994@gmail.com>
2025-08-18 11:36:03 +0500

    chapter_2_completed

---
+++ chapter_2/app.js
@@ -0,0 +1,30 @@
+// 1. Declare a variable called username.
+
+// let num;             // variable Declared
+
+// console.log(num);    //// undefined (agar value nahi di) 
+
+
+
+// 2. Declare a variable called myName & assign to it a string 
+// that represents your Full Name. 
+
+// let num1 = "Majid"
+// console.log(num1);
+
+
+
+// 3. Write script to 
+// a) Declare a JS variable, titled message. 
+// b) Assign “Hello World” to variable message 
+// c) Display the message in alert box. 
+
+
+let num2;
+num2 = "Hello Hassaan"
+alert(num2);
+
+
+
+
+
