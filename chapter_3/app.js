7ec6c507096903c9e3553da6be11498c14c7bc35
Hassaan Ali <hassaanali994@gmail.com>
2025-08-21 11:50:33 +0500

    chapter#3_updated

---
+++ chapter_3/app.js
@@ -0,0 +1,68 @@
+// 1. Declare a variable called age & assign to it your age. Show 
+// your age in an alert box.
+
+
+// let age = 18
+// alert(`I am ${age} years old`)
+
+
+
+
+// 2. Declare & initialize a variable to keep track of how many 
+// times a visitor has visited a web page. Show his/her 
+// number of visits on your web page. For example: “You 
+// have visited this site N times”.
+
+
+// // 1) Key ka naam define karo
+// const KEY = 'visitCount';
+
+// // 2) Purana value parho (string milti hai) -> number banao
+// let count = Number(localStorage.getItem(KEY)) || 0;
+
+// // 3) +1 (kyun ke user ne page phir khola)
+// count += 1;
+
+// // 4) Wapas save karo
+// localStorage.setItem(KEY, String(count));
+
+// // 5) Page par show karo
+// const msg =
+//     `You have visited this site ${count} ` + (count === 1 ? 'time' : 'times') + '.';
+// document.getElementById('visits').textContent = msg;
+
+// // (optional) Alert bhi dikhana ho:
+// // alert(msg);
+
+
+
+
+// 3. Declare a variable called birthYear & assign to it your 
+// birth year. Show the following message in your browser:
+
+
+// let birth = 2008
+// let message1 = `My Birth Day Year is ${birth}`
+
+// document.write(message1)
+
+
+
+
+// 4. A visitor visits an online clothing store 
+// www.xyzClothing.com . Write a script to store in variables 
+// the following information: 
+// a. Visitor’s name
+// b. Product title 
+// c. Quantity i.e. how many products a visitor wants to 
+// order 
+// Show the following message in your browser: “John 
+// Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+
+
+// let customer = "Hassaan"
+// let product = " Air Gordan Shoes"
+// let quantity = 1
+// let sum = customer + " ordered " + quantity + product + " on XYZ Clothing store."
+
+// alert(sum)
