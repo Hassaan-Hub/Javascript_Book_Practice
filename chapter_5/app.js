ac7b76f0d8b135aa651c4989d9cf2b52ae383ab6
Hassaan Ali <hassaanali994@gmail.com>
2025-08-23 18:59:25 +0500

    chapter#5_updated

--- chapter_5/app.js
+++ chapter_5/app.js
@@ -5,9 +5,9 @@
 let num1 = prompt("pehla number lekho")
 let num2 = prompt("dosra number lekho")
 
-num1 = parseFloat(num1)
-num1 = parseFloat(num2)
+num1 = parseFloat(num1);
+num2 = parseFloat(num2);
 
-let result = num1 + num2
+let result = num1 + num2;
 
-document.write("<p>" + num1 + "aur" + num2 + "ka jama hai: " + result +"</p>")
+document.write("<p>" + num1 + " aur " + num2 + " ka jama hai: " + result + "</p>")
