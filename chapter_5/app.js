384f7deef11c48783acb74aa1fd10d420dcd4e8b
Hassaan Ali <hassaanali994@gmail.com>
2025-08-23 18:46:40 +0500

    chapter#5_updated

---
+++ chapter_5/app.js
@@ -0,0 +1,13 @@
+// 1. Write a program that take two numbers & add them in a 
+// new variable. Show the result in your browser. 
+
+
+let num1 = prompt("pehla number lekho")
+let num2 = prompt("dosra number lekho")
+
+num1 = parseFloat(num1)
+num1 = parseFloat(num2)
+
+let result = num1 + num2
+
+document.write("<p>" + num1 + "aur" + num2 + "ka jama hai: " + result +"</p>")
