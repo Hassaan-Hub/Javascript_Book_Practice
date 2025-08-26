d5a2e0604cda7e1a984409a85aaccf63e1c1d03c
Hassaan Ali <hassaanali994@gmail.com>
2025-08-26 23:05:21 +0500

    updated

---
+++ chapter_6-9/app.js
@@ -0,0 +1,29 @@
+// 1. Write a program to take a number in a variable, do the 
+// required arithmetic to display the following result in your 
+// browser: 
+
+
+document.writeln(`<b>Result:</b> <br>`)
+
+var a = 10
+document.writeln(`The value of a is: ${a} <br><br><hr><br>`)
+document.writeln(`The value of ++a is: ${++a} <br>`)
+document.writeln(`Now the value of a is: ${a} <br> <br>`)
+document.writeln(`The value of a++ is: ${a++} <br>`)
+document.writeln(`Now the value of a is: ${a} <br> <br>`)
+document.writeln(`The value of --a is: ${--a} <br>`)
+document.writeln(`Now the value of a is: ${a} <br> <br>`)
+document.writeln(`The value of a-- is: ${a--} <br>`)
+document.writeln(`Now the value of a is: ${a}`)
+
+
+
+
+
+
+
+
+
+
+
+
