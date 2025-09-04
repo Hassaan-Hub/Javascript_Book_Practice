480f67c742d539ec925b593cebacde7b4c647d53
Hassaan Ali <hassaanali994@gmail.com>
2025-09-04 12:40:31 +0500

    updated

---
+++ chapter#17to20/app.js
@@ -0,0 +1,58 @@
+// 1. Declare and initialize an empty multidimensional array. 
+// (Array of arrays)  
+
+
+// let multiArr = [[1, 2], [3, 4], [5, 6]]
+
+// console.log(multiArr);
+
+
+
+
+// 2. Declare and initialize a multidimensional array 
+// representing the following matrix:  
+
+
+// 2D Array (Matrix)
+// let matrix = [
+//     [0, 1, 2, 3],
+//     [1, 0, 1, 2],
+//     [2, 1, 0, 1]
+// ];
+
+// Display matrix
+// console.log(matrix);
+
+
+// // Accessing rows & columns
+// console.log(matrix[0]);     // [0, 1, 2, 3]   → 1st row
+// console.log(matrix[0][3]);  // 1             → row 2, column 3
+
+
+
+
+// 3. Write a program to print numeric counting from 1 to 10.  
+
+
+for(let i = 1; i <=10; i++){
+    console.log(i);
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
