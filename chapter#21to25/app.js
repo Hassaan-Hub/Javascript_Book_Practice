9b637ea42b7b259a068c9d7a8f7bb0c097d916a3
Hassaan Ali <hassaanali994@gmail.com>
2025-09-06 17:25:04 +0500

    Add initial HTML structure and JavaScript exercises for chapters 21 to 25

---
+++ chapter#21to25/app.js
@@ -0,0 +1,66 @@
+// 1. Write a program that takes two user inputs for first and 
+// last name using prompt and merge them in a new variable 
+// titled fullName. Greet the user using his full name. 
+
+
+// let firstName = prompt("Enter your first name")
+// let lastName = prompt("Enter your last name")
+
+// let mergeName = firstName + " " + lastName
+
+// document.writeln(`Hello ${mergeName}! Welcome😊`)
+
+
+
+
+// 2. Write a program to take a user input about his favorite 
+// mobile phone model. Find and display the length of user 
+// input in your browser 
+
+
+// let fav = prompt("Enter your Favorite mobile Name")
+
+// document.writeln(`My favorite phone is: ${fav} <br>`)
+
+// document.writeln(`Length of string: ${fav.length}`)
+
+
+
+
+// 3. Write a program to find the index of letter “n” in the word 
+// “Pakistani” and display the result in your browser . 
+
+
+let arr = "Pakistani"
+
+let index = arr.indexOf("n")
+
+document.writeln(`String: ${arr}<br>`)
+document.writeln(`index of 'n': ${index}`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
