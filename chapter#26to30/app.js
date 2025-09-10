c2aa4396c59cb995a3e76724a8a4821413eb8a25
Hassaan Ali <hassaanali994@gmail.com>
2025-09-10 20:55:07 +0500

    Add initial HTML structure and JavaScript program for number manipulation exercises

---
+++ chapter#26to30/app.js
@@ -0,0 +1,73 @@
+// 1. Write a program that takes a positive integer from user & 
+// display the following in your browser. 
+// a. number 
+// b. round off value of the number 
+// c. floor value of the number 
+// d. ceil value of the number 
+
+
+// let number = prompt("Enter your number")
+
+// document.write(`Number: ${number}<br>`)
+
+// document.write(`Round off value: ${Math.round(number)}<br>`)
+
+// document.write(`floor value off: ${Math.floor(number)}<br>`)
+
+// document.write(`ceil value off: ${Math.ceil(number)}`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
