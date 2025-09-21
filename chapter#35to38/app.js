b8d5738db91c83dec67238ffadccdc6ff87074a0
Hassaan Ali <hassaanali994@gmail.com>
2025-09-21 13:09:41 +0500

    Add calculator function to perform operations based on user input

---
+++ chapter#35to38/app.js
@@ -0,0 +1,62 @@
+// 1. Write a function that displays current date & time in your 
+// browser. 
+
+
+// function currentDate(){
+//     let now = new Date()
+
+//     document.getElementById("currentDate").innerHTML = now;
+// }
+// currentDate()
+
+
+
+
+// 2. Write a function that takes first & last name and then it 
+// greets the user using his full name. 
+
+
+// function greetUser(firstName , lastName){
+//     let fullName = firstName + " " + lastName
+
+//     document.getElementById("greeting").innerHTML = "Hello " + fullName + "!"
+
+// }
+
+// let greet = prompt("enter your first name")
+// let greet1 = prompt("enter your last name")
+
+// greetUser(greet , greet1)
+
+
+
+
+// 3. Write a function that adds two numbers (input by user) 
+// and returns the sum of two numbers. 
+
+
+// function addTwoNumbers(){
+//     let num = +prompt("Enter your first Number")
+//     let num1 = +prompt("Enter your last Number")
+
+//     let sum = num + num1
+
+//     return sum;
+// }
+
+// let total = addTwoNumbers()
+// document.getElementById("result").innerHTML = "The sum number is " + total
+
+
+
+
+// 4. Calculator:  
+// Write a function that takes three arguments num1, num2 
+// & operator & compute the desired operation. Return and 
+// show the desired result in your browser. 
+
+
+function calculator(num1, num2, operator){
+    let return;
+    
+}
