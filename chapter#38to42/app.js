640c5440a664942cad444ab095f4648d6da07d11
Hassaan Farid <hassaanali994@gmail.com>
2025-12-30 13:01:51 +0500

    Add initial implementation of power function and create index.html for chapter #38to42

---
+++ chapter#38to42/app.js
@@ -0,0 +1,92 @@
+// 1. Write a custom function power ( a, b ), to calculate the value of
+// a raised to b.
+
+
+// function power(a, b) {
+//     let result = 1;
+//     for (let i = 0; i < b; i++) {
+//         result = result * a;
+//     }    
+//     return result;
+// }
+
+// console.log(power(2, 4));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
