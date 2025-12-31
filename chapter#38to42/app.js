28693e40c52795209d00dbfe4c6b9044700d5781
Hassaan Farid <hassaanali994@gmail.com>
2025-12-31 19:01:57 +0500

    Refactor comments in app.js for clarity and consistency; update leap year function description

--- chapter#38to42/app.js
+++ chapter#38to42/app.js
@@ -6,7 +6,7 @@
 //     let result = 1;
 //     for (let i = 0; i < b; i++) {
 //         result = result * a;
-//     }    
+//     }
 //     return result;
 // }
 
@@ -15,29 +15,34 @@
 
 
 
+// 2. Any year is entered through the keyboard. Write a function to
+// determine whether the year is a leap year or not.
+// Leap years ..., 2012, 2016, 2020, …
+
+
+// function checkLeapYear(year){
+//     if(year % 4 === 0){
+//         document.write(`${year} is a leap year`)
+//     } else if(year % 400 === 0){
+//         document.write(`${year} is a leap1 year`)
+//     } else if(year % 100 === 0){
+//         document.write(`${year} is a not leap year`)
+//     } else{
+//         document.write(`${year} is a not leap year`)
+//     }
+// }
+
+// var year1 = prompt("Enter a year")
+// checkLeapYear(year1)
+        
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// 3. If the lengths of the sides of a triangle are denoted by a, b, and
+// c, then area of triangle is given by
+// area = S(S − a)(S − b)(S − c)
+// where, S = ( a + b + c ) / 2
+// Calculate area of triangle using 2 functions
 
 
 
