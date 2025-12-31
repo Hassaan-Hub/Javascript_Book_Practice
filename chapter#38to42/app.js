3ad5a740b8571899c9a6468d269a97ded468749c
Hassaan Farid <hassaanali994@gmail.com>
2025-12-31 22:58:44 +0500

    updated

--- chapter#38to42/app.js
+++ chapter#38to42/app.js
@@ -1,7 +1,6 @@
 // 1. Write a custom function power ( a, b ), to calculate the value of
 // a raised to b.
 
-
 // function power(a, b) {
 //     let result = 1;
 //     for (let i = 0; i < b; i++) {
@@ -12,13 +11,9 @@
 
 // console.log(power(2, 4));
 
-
-
-
 // 2. Any year is entered through the keyboard. Write a function to
 // determine whether the year is a leap year or not.
 // Leap years ..., 2012, 2016, 2020, …
-
 
 // function checkLeapYear(year){
 //     if(year % 4 === 0){
@@ -34,9 +29,6 @@
 
 // var year1 = prompt("Enter a year")
 // checkLeapYear(year1)
-        
-
-
 
 // 3. If the lengths of the sides of a triangle are denoted by a, b, and
 // c, then area of triangle is given by
@@ -44,54 +36,11 @@
 // where, S = ( a + b + c ) / 2
 // Calculate area of triangle using 2 functions
 
+// function triangleArea(a, b, c){
+//     let S = (a + b + c) / 2;
+//     var area = Math.sqrt(S * (S - a) * (S - b) * (S - c))
+//     return area;
+// }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// var result = triangleArea(4, 5, 7)
+// document.write(`Area of triangle: ${result}`)
