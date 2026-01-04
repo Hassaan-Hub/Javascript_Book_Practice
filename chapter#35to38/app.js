319b4397909c01223d58d8121eb4d005e8848173
Hassaan Farid <hassaanali994@gmail.com>
2026-01-04 16:13:13 +0500

    Add letterCount function to count occurrences of a specified letter in a string

--- chapter#35to38/app.js
+++ chapter#35to38/app.js
@@ -236,8 +236,24 @@
 
 
 
+// 13. Write a JavaScript function that accepts two arguments, a
+// string and a letter and the function will count the number of
+// occurrences of the specified letter within the string.
+// Sample arguments : 'JSResourceS.com', 'o'
 
 
+// function letterCount(str, letter){
+//     var count = 0;
+
+//     for(var i = 0; i < str.length; i++){
+//         if(str[i] === letter){
+//             count++
+//         }
+//     }
+//     return count;
+// } 
+// var result = letterCount("JSResourceS.com", "o")
+// console.log(result);
 
 
 
