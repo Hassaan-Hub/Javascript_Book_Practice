431bb3496acd296a5c44ba8284e88f0b97fb07a7
Hassaan Farid <hassaanali994@gmail.com>
2026-01-20 21:59:29 +0500

    updated

--- chapter#49to52/app.js
+++ chapter#49to52/app.js
@@ -10,3 +10,32 @@
 //     "Name: " + name + "<br>Email: " + email + "<br><br><br>";
 
 // }
+
+
+
+
+// 2. Suppose in your webpage there is content area in which
+// you have entered your item details, but user can only see
+// some details on first look. When user clicks on “Read
+// more” button, full detail of that particular item will be
+// displayed.
+
+
+// function readMore(){
+//     document.getElementById("para").innerHTML = `  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit at
+//       incidunt blanditiis, distinctio possimus molestiae, quis repellendus
+//       mollitia voluptatem provident ex ipsa? Quos dolore non tenetur quasi
+//       voluptatibus qui culpa!
+//     `
+// }
+
+
+
+
+
+
+
+
+
+
+
